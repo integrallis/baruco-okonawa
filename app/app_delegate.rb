57a63639270a645b0893a751f5b35c98e704a0ce
Brian Sam-Bodden <bsbodden@integrallis.com>
2014-09-08 14:44:45 -0700

    Passing Spec: App - has one window

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    true
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    #@window = UIWindow.alloc.init
+    #@window = UIWindow.new
+    @window.makeKeyAndVisible
   end
 end
