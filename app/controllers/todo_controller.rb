86e4c650bd7fac45c68e46b8ee83edc8800c512c
Brian Sam-Bodden <bsbodden@integrallis.com>
2014-09-10 01:25:48 +0200

    Add 'Save' button to TodoController

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,6 +8,14 @@ class TodoController < Formotion::FormController
     self.todo = todo
   end
 
+  def viewDidLoad
+    super
+    saveButton = UIBarButtonItem.alloc.initWithTitle("Save",
+                   style: UIBarButtonItemStyleBordered,
+                   target: self, action: 'save')
+    self.navigationItem.rightBarButtonItem = saveButton
+  end
+
   def save
     data = @form.render
     @todo.from_formotion!(data)
