9f9686911a1baff96f32e7fa54b772f3db97f5f5
Brian Sam-Bodden <bsbodden@integrallis.com>
2014-09-08 17:08:17 -0700

    Refactor: Use UITableViewController instead of usings views directly in delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,21 +1,16 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    @todos_controller = TodosController.alloc
+                        .initWithNibName(nil, bundle:nil)
+
+    @window.rootViewController =
+      UINavigationController.alloc
+        .initWithRootViewController(@todos_controller)
+
     @window.makeKeyAndVisible
 
-    @table = UITableView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-
-    todos = %w(Milk Orange\ Juice Apples Bananas Broccoli Carrots Beef
-               Chicken Enchiladas Hot\ Dogs Butter Bread Pasta Rice)
-
-    todos.map! { |thing| "Buy #{thing}"}
-
-    @data_source = TodosDataSource.new
-
-    @data_source.data = todos
-
-    @table.dataSource = @data_source
-
-    @window.addSubview(@table)
+    true
   end
 end
---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,26 @@
+class TodosController < UIViewController
+  attr_writer :data
+
+  def viewDidLoad
+    super
+    self.title = 'Okonawa'
+    @table = UITableView.alloc.initWithFrame(self.view.bounds)
+    @table.dataSource = self
+    self.view.addSubview(@table)
+
+    @data = %w(Milk Orange\ Juice Apples Bananas Broccoli Carrots Beef
+               Chicken Enchiladas Hot\ Dogs Butter Bread Pasta Rice)
+               .map { |thing| "Buy #{thing}" }
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @data.size
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault,
+                                                         reuseIdentifier:nil)
+    cell.textLabel.text = @data[indexPath.row]
+    cell
+  end
+end
